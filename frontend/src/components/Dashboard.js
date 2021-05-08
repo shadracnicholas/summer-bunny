a4866ba1ebfcac4d7e1786cb2e127ffacb79fd4e
SHADRACK NICHOLAS <46599542+shadracnicholas@users.noreply.github.com>
2021-05-09 01:48:20 +0300

    Implemented Logout logic

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,7 +1,34 @@
+import React, { useState } from "react"
+import { useAuth } from "../contexts/AuthContext"
+import { useHistory } from "react-router-dom"
+
 export default function Dashboard() {
+
+    const [error, setError] = useState("")
+    const { currentUser, logout } = useAuth()
+    const history = useHistory()
+
+    async function handleLogout() {
+        setError("")
+
+        try {
+        await logout()
+        history.push("/login")
+        } catch {
+        setError("Failed to log out")
+        }
+    }
+
     return (
         <div>
-            <h1>Dashboard</h1>
+            {error && <h3 className="text-sm font-medium text-red-800">{error}</h3>}
+
+            <h1>Welcome, {currentUser.email}</h1>
+            <button
+                onClick={handleLogout}
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500">
+                Logout
+            </button>
         </div>
     )
 }
