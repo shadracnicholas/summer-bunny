6e067680d54ad0c12ffbeb061dbcc85984419c49
SHADRACK NICHOLAS <46599542+shadracnicholas@users.noreply.github.com>
2021-05-09 01:23:12 +0300

    Add Dashboard as a Protected route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,20 @@
-import Login from "./Login";
+import Login from "./Login"
+import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Dashboard from "./Dashboard"
+import PrivateRoute from "./PrivateRoute"
 
 function App() {
-  return <Login />
+  return (
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <PrivateRoute exact path="/" component={Dashboard} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </AuthProvider>
+    </Router>
+  )
 }
 
 export default App;
