a71dbfb401d5f89da8e0a7a65ed269d8c09d0462
SHADRACK NICHOLAS <46599542+shadracnicholas@users.noreply.github.com>
2021-05-09 01:43:03 +0300

    Frontend Authentication with Firebase

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,31 @@
+import React, { useRef, useState } from "react"
+import { useAuth } from "../contexts/AuthContext"
+import { useHistory } from "react-router-dom"
+
 export default function Login() {
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const { login } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setError("")
+      setLoading(true)
+      await login(emailRef.current.value, passwordRef.current.value)
+      history.push("/")
+    } catch {
+      setError("Failed to log in")
+    }
+
+    setLoading(false)
+  }
+
   return (
     <div className="min-h-screen bg-white flex">
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
@@ -15,7 +42,10 @@ export default function Login() {
           <div className="mt-8">
 
             <div className="mt-6">
-              <form action="#" method="POST" className="space-y-6">
+
+              {error && <h3 className="text-sm font-medium text-red-800">{error}</h3>}
+              
+              <form onSubmit={handleSubmit} className="space-y-6">
                 <div>
                   <label htmlFor="email" className="block text-sm font-medium text-gray-700">
                     Email address
@@ -23,6 +53,7 @@ export default function Login() {
                   <div className="mt-1">
                     <input
                       id="email"
+                      ref={emailRef}
                       name="email"
                       type="email"
                       autoComplete="email"
@@ -39,6 +70,7 @@ export default function Login() {
                   <div className="mt-1">
                     <input
                       id="password"
+                      ref={passwordRef}
                       name="password"
                       type="password"
                       autoComplete="current-password"
@@ -53,6 +85,13 @@ export default function Login() {
                     type="submit"
                     className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md text-sm font-medium text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
                   >
+                    { loading && 
+                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx={12} cy={12} r={10} stroke="currentColor" strokeWidth={4} />
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
+                    </svg> 
+                    }
+                    
                     Sign in
                   </button>
                 </div>
