b779a382272011a8298689300eeb55c36b879cd8
SHADRACK NICHOLAS <46599542+shadracnicholas@users.noreply.github.com>
2021-05-09 01:15:29 +0300

    Create Authentication Context

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
-import Signup from "./Signup";
+import Login from "./Login";
 
 function App() {
-  return <Signup />
+  return <Login />
 }
 
 export default App;
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,4 @@
-export default function Signup() {
+export default function Login() {
   return (
     <div className="min-h-screen bg-white flex">
       <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
